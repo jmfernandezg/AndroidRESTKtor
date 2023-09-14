9d85c5ef4886ee155f2f38b44dccb1ed93111054
Jose Fernandez <jmfernandezg.awe@gmail.com>
2023-09-13 20:26:51 -0600

    First commit :tada:

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # AndroidRESTKtor
 Training for Android App with REST API and Ktor Server
+
+
+## Section 1 : Introduction
+
+### 1.1 Introduction
+
+- Ktor is a framework for building asynchronous servers and clients in connected systems using the powerful Kotlin programming language.
+- Ktor is built from the ground up using coroutines.
+- For Android we will use JetPack Compose
+- We will use retrofit and paging library
+
+### 2: Repository and resources 
+
+- Two Projects: Backend server and android application
+
+---
+
+## Section 2 : Ktor Server
+
---
+++ app/build.gradle.kts
@@ -0,0 +1,59 @@
+val ktorVersion: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
+val koinVersion: String by project
+val guavaVersion: String by project
+
+plugins {
+    application
+    kotlin("jvm") version "1.9.10"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10"
+}
+
+group = "com.jmfg.training"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.google.guava:guava:$guavaVersion")
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+
+    implementation("io.ktor:ktor-server-call-logging:$ktorVersion")
+    implementation("io.ktor:ktor-server-status-pages:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-server-default-headers:$ktorVersion")
+
+    testImplementation("io.ktor:ktor-server-tests:$ktorVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlinVersion")
+
+    implementation("io.insert-koin:koin-ktor:$koinVersion")
+    implementation("io.insert-koin:koin-logger-slf4j:$koinVersion")
+}
+
+testing {
+    suites {
+        // Configure the built-in test suite
+        val test by getting(JvmTestSuite::class) {
+            // Use Kotlin Test test framework
+            useKotlinTest("1.9.10")
+        }
+    }
+}
+
+// Apply a specific Java toolchain to ease working on different environments.
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+application {
+    // Define the main class for the application.
+    mainClass.set("com.jmfg.training.ktor.AppKt")
+}
